c51c7facbe4b6d66ca8014b584847bea6f7f8b25
Petrus Pierre <petrus.tsx@gmail.com>
2023-02-17 09:24:01 -0300

    unique entity specs

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -4,9 +4,13 @@ import { UniqueEntityId } from "./unique-entity-id.vo"
 import { InvalidUuidError } from '../errors/invalid-uuid.error';
 
 describe('UniqueEntityId unit tests', () => {
-  it('should throw error when uuid is invalid', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
 
+  beforeEach(() => {
+    validateSpy.mockClear()
+  })
+
+  it('should throw error when uuid is invalid', () => {
     expect(() => {
       new UniqueEntityId('fake id')
     }).toThrow(new InvalidUuidError())
@@ -14,8 +18,6 @@ describe('UniqueEntityId unit tests', () => {
   })
 
   it('should accept a uuid passed in constructor', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
-
     const uuid = 'c1d2f6e8-7b9a-4d3c-8e2e-6f9d1a7b8c0d'
     const vo = new UniqueEntityId(uuid)
 
@@ -24,7 +26,6 @@ describe('UniqueEntityId unit tests', () => {
   })
 
   it('should generate a uuid when not passed in constructor',()  => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
     const vo = new UniqueEntityId()
 
     expect(vo.id).toBeDefined()
