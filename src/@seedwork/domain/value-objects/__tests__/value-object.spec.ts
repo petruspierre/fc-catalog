ea2897d282b8abbadcb0396047b0f89eb1563299
Petrus Pierre <petrus.tsx@gmail.com>
2023-02-18 20:17:48 +0000

    feat: convert value object to a string

--- src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -12,4 +12,23 @@ describe('ValueObject Unit Tests', () => {
     vo = new StubValueObject({ prop1: 'value1' })
     expect(vo.value).toStrictEqual({ prop1: 'value1' })
   })
+
+  it('should convert to a string', () => {
+    const date = new Date()
+    let arrange = [
+      { value: 'string value', expected: 'string value' },
+      { value: { prop1: 'value1' }, expected: '{"prop1":"value1"}' },
+      { value: null, expected: 'null' },
+      { value: undefined, expected: 'undefined' },
+      { value: date, expected: date.toString() },
+      { value: 1, expected: '1' },
+      { value: true, expected: 'true' },
+      { value: false, expected: 'false' },
+    ]
+
+    arrange.forEach(({ value, expected }) => {
+      const vo = new StubValueObject(value)
+      expect(`${vo}`).toBe(expected)
+    })
+  })
 })
--- src/@seedwork/domain/value-objects/value-object.ts
+++ src/@seedwork/domain/value-objects/value-object.ts
@@ -8,4 +8,18 @@ export default abstract class ValueObject<Value = any> {
   get value(): Value {
     return this._value;
   }
+
+  toString = () => {
+    if (typeof this.value !== "object" || this.value === null) {
+      try {
+        return this.value.toString();
+      } catch(e) {
+        return this.value + "";
+      }
+    }
+    const valueString = this.value.toString()
+    return valueString === "[object Object]"
+      ? JSON.stringify(this.value) 
+      : valueString;
+  }
 }
