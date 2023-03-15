7f6c6f682b928777de0465cc047d22cd368e45f8
Petrus Pierre <petrus.tsx@gmail.com>
2023-03-15 18:05:30 -0300

    add class validator

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,6 +1,7 @@
 import Entity from '../../../@seedwork/domain/entity/entity';
 import { UniqueEntityId } from '../../../@seedwork/domain/value-objects/unique-entity-id.vo';
 import ValidatorRules from '../../../@seedwork/validators/validator-rules';
+import CategoryValidatorFactory from '../validators/category.validator';
 
 export type CategoryProperties = {
   name: string;
@@ -29,10 +30,9 @@ export class Category extends Entity<CategoryProperties> {
     this.description = description;
   }
 
-  static validate(props: Omit<CategoryProperties, 'created_at'>) {
-    ValidatorRules.values(props.name, 'name').required().string().maxLength(255)
-    ValidatorRules.values(props.description, 'description').string()
-    ValidatorRules.values(props.is_active, 'is_active').boolean()
+  static validate(props: CategoryProperties) {
+    const validator = CategoryValidatorFactory.create()
+    validator.validate(props)
   }
 
   activate() {
