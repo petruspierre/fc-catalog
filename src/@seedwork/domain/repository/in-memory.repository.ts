83b6878bf8500506dda070bcc35251b86116a690
Petrus Pierre <petrus.tsx@gmail.com>
2023-03-19 20:36:59 -0300

    feat: add in-memory repositories

---
+++ src/@seedwork/domain/repository/in-memory.repository.ts
@@ -0,0 +1,47 @@
+import Entity from "../entity/entity";
+import { NotFoundError } from "../errors/not-found.error";
+import { UniqueEntityId } from "../value-objects/unique-entity-id.vo";
+import { RepositoryInterface } from "./repository-contracts";
+
+export default abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+
+  async findById(id: string | UniqueEntityId): Promise<E> {
+    return this._get(`${id}`);
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id);
+
+    const index = this.items.findIndex((i) => i.id === entity.id);
+
+    this.items[index] = entity;
+  }
+
+  async delete(id: string | UniqueEntityId): Promise<void> {
+    const _id = `${id}`;
+    await this._get(_id);
+
+    this.items = this.items.filter((i) => i.id !== _id);
+  }
+
+  protected async _get(id: string): Promise<E> {
+    const item = this.items.find((i) => i.id === id);
+
+    if (!item) {
+      throw new NotFoundError(`Entity not found using ID ${id}`);
+    }
+
+    return item;
+  }
+}
