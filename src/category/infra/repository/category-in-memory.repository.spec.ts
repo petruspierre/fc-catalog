1ce01af37c9d5dc4213636949da908d05e9009ec
Petrus Pierre <petrus.tsx@gmail.com>
2023-03-31 18:28:47 +0000

    feat: add category repository in memory

---
+++ src/category/infra/repository/category-in-memory.repository.spec.ts
@@ -0,0 +1,82 @@
+import { SearchParams, SearchResult } from "../../../@seedwork/domain/repository/repository-contracts";
+import { Category } from "../../domain/entities/category";
+import CategoryInMemoryRepository from "./category-in-memory.repository"
+
+describe('CategoryInMemoryRepository Unit Tests', () => {
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+  })
+
+  describe('search category', () => {
+    it('should paginate result', async () => {
+      repository.items = [new Category({
+        name: 'Category 1',
+      }), new Category({
+        name: 'Category 2',
+      })];
+  
+      const result = await repository.search(new SearchParams({
+        page: 1,
+        per_page: 1
+      }))
+
+      expect(result).toStrictEqual(new SearchResult({
+        current_page: 1,
+        filter: null,
+        items: [repository.items[0]],
+        per_page: 1,
+        sort: null,
+        sort_dir: null,
+        total: 2,
+      }));
+    })
+
+    it('should filter category by name', async () => {
+      repository.items = [new Category({
+        name: 'Category 1',
+      }), new Category({
+        name: 'Category 2',
+      })];
+  
+      const result = await repository.search(new SearchParams({
+        filter: 'Category',
+      }))
+
+      expect(result.items).toStrictEqual([repository.items[0], repository.items[1]]);
+    })
+
+    it('should sort category by name', async () => {
+      repository.items = [new Category({
+        name: 'Category 1',
+      }), new Category({
+        name: 'Category 2',
+      })];
+  
+      const result = await repository.search(new SearchParams({
+        sort: 'name',
+        sort_dir: 'desc',
+      }))
+
+      expect(result.items).toStrictEqual([repository.items[1], repository.items[0]]);
+    })
+
+    it('should sort by created_at by default', async () => {
+      repository.items = [new Category({
+        name: 'Category 1',
+        created_at: new Date('2021-01-01'),
+      }), new Category({
+        name: 'Category 2',
+        created_at: new Date('2022-01-01'),
+      })];
+  
+      const result = await repository.search(new SearchParams({
+        sort: null,
+        sort_dir: null,
+      }))
+
+      expect(result.items).toStrictEqual([repository.items[1], repository.items[0]]);
+    })
+  })
+})
--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,7 +1,36 @@
-import { InMemorySearchableRepository } from '../../../@seedwork/domain/repository/in-memory.repository';
+import { SortDirection } from "@seedwork/domain/repository/repository-contracts";
+import { InMemorySearchableRepository } from "../../../@seedwork/domain/repository/in-memory.repository";
 import { Category } from "../../domain/entities/category";
 import CategoryRepository from "../../domain/repository/category.repository";
 
 export default class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
-  implements CategoryRepository {}
+  implements CategoryRepository.Repository
+{
+  sortableFields: string[] = ["name", "created_at"];
+
+  protected async applyFilter(
+    items: Category[],
+    filter: CategoryRepository.Filter
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((i) => {
+      return i.props.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
+  protected async applySort(
+    items: Category[],
+    sort: string,
+    sort_dir: SortDirection
+  ): Promise<Category[]> {
+    if (!sort) {
+      return super.applySort(items, "created_at", "desc");
+    } else {
+      return super.applySort(items, sort, sort_dir);
+    }
+  }
+}
