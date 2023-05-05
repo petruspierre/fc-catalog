fcfe8a4d0d25407bf8ffb4623b6188443d6611a8
Petrus Pierre <petrus.tsx@gmail.com>
2023-05-05 00:04:13 -0300

    feat: registering use cases in nestjs

---
+++ src/nestjs/src/categories/categories.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CategoriesController } from './categories.controller';
+import {
+  CreateCategoryUseCase,
+  ListCategoriesUseCase,
+} from '@pp/core/category/application';
+import { CategoryInMemoryRepository } from '@pp/core/category/infra';
+import { CategoryRepository } from '@pp/core/category/domain';
+
+@Module({
+  controllers: [CategoriesController],
+  providers: [
+    CategoriesService,
+    {
+      provide: 'CategoryInMemoryRepository',
+      useClass: CategoryInMemoryRepository,
+    },
+    {
+      provide: CreateCategoryUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new CreateCategoryUseCase.UseCase(categoryRepo);
+      },
+      inject: ['CategoryInMemoryRepository'],
+    },
+    {
+      provide: ListCategoriesUseCase.UseCase,
+      useFactory: (categoryRepo: CategoryRepository.Repository) => {
+        return new ListCategoriesUseCase.UseCase(categoryRepo);
+      },
+      inject: ['CategoryInMemoryRepository'],
+    },
+  ],
+})
+export class CategoriesModule {}
